1088b6972655f2f0ecf521628941a2be0a1ebcf7
Amanda Claproth <amanda.claproth@gmail.com>
2020-11-03 13:36:30 -0800

    add main page

--- src/pages/Main/Main.js
+++ src/pages/Main/Main.js
@@ -3,10 +3,16 @@ import './Main.css';
 
 function Main() {
     return (
-        <div id="main">
-            <p>Main</p>
-        </div>
-    )
+        <>
+            <div id="main">
+                <h3>H E L L O</h3> <br />
+                <p> Welcome and thanks for checking out my portfolio! <br />
+                Click through the navigation above and to the right to learn more. <br />
+                </p>
+                <img src="https://i.imgur.com/kIQBOxX.jpg" width="40%" height="40%" className="photo"></img>
+            </div>
+        </>
+    );
 }
 
 export default Main;
